498f90534f38e70351eb41a442c84cf2400a0606
Juan Cruz Sosa <nirvguy@gmail.com>
2018-04-15 00:53:14 -0300

    Add Averager meter

--- torchtrainer/meters.py
+++ torchtrainer/meters.py
@@ -80,3 +80,28 @@ class CategoricalAccuracy(BaseMeter):
             return self.result / self.num_samples
         else:
             return self.result * 100.0 / self.num_samples
+
+class Averager(BaseMeter):
+    """ Meter that returns the average over all measured values
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.result = None
+        self.num_samples = 0
+
+    def measure(self, value):
+        if self.result is None:
+            self.result = value
+        else:
+            self.result += value
+
+        self.num_samples += 1
+
+    def value(self):
+        if self.num_samples == 0:
+            raise ZeroMeasurementsError()
+
+        return self.result / self.num_samples
