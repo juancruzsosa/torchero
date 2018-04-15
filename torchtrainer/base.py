1b04b5e0196f7c889495ca609b4ecbe0f5c9b64a
Juan Cruz Sosa <nirvguy@gmail.com>
2018-04-15 00:41:19 -0300

    Move HookContainer to hooks module

--- torchtrainer/base.py
+++ torchtrainer/base.py
@@ -1,29 +1,8 @@
 import torch
 from torch.autograd import Variable
 from abc import abstractmethod
-from collections import deque
-from .hooks import Hook
+from .hooks import HookContainer
 
-class HookContainer(Hook):
-    def __init__(self, trainer):
-        super(HookContainer, self).__init__()
-        self.accept(trainer)
-        self.hooks = deque()
-
-    def attach(self, hook):
-        hook.accept(self.trainer)
-        self.hooks.append(hook)
-
-    def signal(self, selector_name):
-        for hook in self.hooks:
-            hook_method = getattr(hook, selector_name)
-            hook_method()
-
-    def pre_epoch(self):
-        self.signal('pre_epoch')
-
-    def post_epoch(self):
-        self.signal('post_epoch')
 
 class BaseTrainer(object):
     """ Base Trainer for all Trainer classes.
--- torchtrainer/hooks.py
+++ torchtrainer/hooks.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Hook(object):
     """ Hooks for epoch/batch training events
     """
@@ -23,3 +25,24 @@ class Hook(object):
         """ Trigger called after every epoch
         """
         pass
+
+class HookContainer(Hook):
+    def __init__(self, trainer):
+        super(HookContainer, self).__init__()
+        self.accept(trainer)
+        self.hooks = deque()
+
+    def attach(self, hook):
+        hook.accept(self.trainer)
+        self.hooks.append(hook)
+
+    def signal(self, selector_name):
+        for hook in self.hooks:
+            hook_method = getattr(hook, selector_name)
+            hook_method()
+
+    def pre_epoch(self):
+        self.signal('pre_epoch')
+
+    def post_epoch(self):
+        self.signal('post_epoch')
