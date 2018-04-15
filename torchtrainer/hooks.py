d315b01cf0569b3c8d270f51ff8c7a9f6ff2cc27
Juan Cruz Sosa <nirvguy@gmail.com>
2018-04-15 01:03:40 -0300

    Add History hook

--- torchtrainer/hooks.py
+++ torchtrainer/hooks.py
@@ -55,3 +55,13 @@ class HookContainer(Hook):
     def log(self):
         self.signal('log')
 
+class History(Hook):
+    def __init__(self):
+        super(History, self).__init__()
+        self.registry = []
+
+    def log(self):
+        line = {'epoch' : self.trainer.epoch,
+                'step' : self.trainer.step}
+        line.update(self.trainer.last_stats)
+        self.registry.append(line)
