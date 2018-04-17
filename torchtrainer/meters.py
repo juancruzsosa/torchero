3186c4bfff9c6e1596ff5f9e6c4327051a292ed4
Juan Cruz Sosa <nirvguy@gmail.com>
2018-04-17 00:48:49 -0300

    Add MSE metric

--- torchtrainer/meters.py
+++ torchtrainer/meters.py
@@ -1,4 +1,5 @@
 import torch
+import math
 from abc import abstractmethod
 from enum import Enum
 import copy
@@ -84,6 +85,48 @@ class CategoricalAccuracy(BaseMeter):
             return self.result / self.num_samples
         else:
             return self.result * 100.0 / self.num_samples
+
+class MSE(BaseMeter):
+    """ Meter for mean squared error metric
+    """
+
+    INVALID_BATCH_DIMENSION_MESSAGE = 'Expected both tensors have at less two dimension and same shape'
+    INVALID_INPUT_TYPE_MESSAGE = 'Expected types (FloatTensor, FloatTensor) as inputs'
+
+    def __init__(self, take_sqrt=False):
+        """ Constructor
+
+        Arguments:
+            take_sqrt (bool): Take square root in final results
+        """
+        self.take_sqrt = take_sqrt
+        self.reset()
+
+    def reset(self):
+        self.result = 0.0
+        self.num_samples = 0
+
+    def _get_result(self, a, b):
+        return torch.pow(a-b, 2)
+
+    def measure(self, a, b):
+        if not torch.is_tensor(a) or not torch.is_tensor(b):
+            raise TypeError(self.INVALID_INPUT_TYPE_MESSAGE)
+
+        if len(a.size()) != 2 or b.shape != a.shape:
+            raise ValueError(self.INVALID_BATCH_DIMENSION_MESSAGE)
+
+        self.result += torch.sum(self._get_result(a, b))
+        self.num_samples += len(b)
+
+    def value(self):
+        if self.num_samples == 0:
+            raise ZeroMeasurementsError()
+
+        result = self.result / self.num_samples
+        if self.take_sqrt:
+            result = math.sqrt(result)
+        return result
 
 class Averager(BaseMeter):
     """ Meter that returns the average over all measured values
