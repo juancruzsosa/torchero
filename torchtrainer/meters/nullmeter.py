a819626c551581aaccda86634b65a1b6736c4c23
Juan Cruz Sosa <nirvguy@gmail.com>
2018-04-17 20:22:26 -0300

    Makes NullMeter singleton

--- torchtrainer/meters/nullmeter.py
+++ torchtrainer/meters/nullmeter.py
@@ -1,11 +1,24 @@
 from .base import BaseMeter, ZeroMeasurementsError
 
 class NullMeter(BaseMeter):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
+
     def measure(self, *batchs):
         pass
 
     def reset(self):
         pass
 
+    def clone(self):
+        return self
+
     def value(self):
         raise ZeroMeasurementsError()
+
+    def __repr__(self):
+        return "NullMeter()"
