d21fa2b19ad221199a1d88a67dac971147441be0
Juan Cruz Sosa <nirvguy@gmail.com>
2018-04-20 11:33:59 -0300

    Add Maximum/Minimum aggregators

--- torchtrainer/meters/aggregators/batch.py
+++ torchtrainer/meters/aggregators/batch.py
@@ -20,3 +20,23 @@ class Average(Sum):
     """
     def final_value(self, result):
         return super(Average, self).final_value(result) / self.num_samples
+
+class Maximum(BatchAggregator):
+    """ Aggregator that returns the maximum of all given batchs
+    """
+    def initial_value(self):
+        return None
+
+    def combine_batch(self, old_result, value):
+        value = torch.max(value)
+        return value if old_result is None else max(old_result, value)
+
+class Minimum(BatchAggregator):
+    """ Aggregator that returns the minimum of all given batchs
+    """
+    def initial_value(self):
+        return None
+
+    def combine_batch(self, old_result, value):
+        value = torch.min(value)
+        return value if old_result is None else min(old_result, value)
