011cc0ab4c0c916382a6927641e3375b8642b8e0
Juan Cruz Sosa <nirvguy@gmail.com>
2018-04-30 15:22:59 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='torchtrainer',
+      version='0.0.1',
+      description='A pluggable & extensible trainer for pytorch',
+      url='http://github.com/nirvguy/torchtrainer',
+      author='Juan Cruz Sosa',
+      author_email='nirvguy@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'torch',
+          'pyyaml'
+      ],
+      extra_requires={
+        'monitor training/validation with progress bar support' : ['tqdm']
+      },
+      zip_safe=False)
