65d7964c8d9f44c551db6fbe80ba287149ad6a55
Juan Cruz Sosa <nirvguy@gmail.com>
2018-05-11 13:01:24 -0300

    Add valid_dataset argument in CrossFoldValidation splitter

--- torchtrainer/utils/data/cross_fold_validation.py
+++ torchtrainer/utils/data/cross_fold_validation.py
@@ -9,13 +9,21 @@ class CrossFoldValidation(object):
 
     INVALID_VALID_SIZE_MESSAGE = ('Invalid `valid_size`: `valid_size` must lay '
                                   'between 0.0 and 1.0')
+    TRAIN_AND_VALID_DATASET_SIZE_MESSAGE = ('Train and valid dataset must have '
+                                            'same size')
 
-    def __init__(self, dataset, valid_size=0.2):
+    def __init__(self, dataset, valid_dataset=None, valid_size=0.2):
         """ Constructor
 
         Args:
             dataset (`torch.utils.data.Dataset`):
                 Dataset to split
+            valid_dataset (`torch.utils.data.Dataset`):
+                Validation dataset where to take validation samples. Should be
+                the same dataset as the training dataset but possibly with
+                different transformations (for example training dataset is
+                augmented and validation is not). If None the `dataset'
+                argument its used (default).
             valid_size (float):
                 Proportion of datset destined to validation.
                 Must lay between 0 and 1
@@ -25,8 +33,12 @@ class CrossFoldValidation(object):
         if valid_size <= 0 or valid_size >= len(dataset):
             raise Exception(self.INVALID_VALID_SIZE_MESSAGE)
 
+        if valid_dataset is not None and len(valid_dataset) != len(dataset):
+            raise Exception(self.TRAIN_AND_VALID_DATASET_SIZE_MESSAGE)
+
         self.valid_size = valid_size
         self.dataset = dataset
+        self.valid_dataset = valid_dataset or dataset
         self.indices = list(torch.randperm(len(dataset)))
 
     def __len__(self):
@@ -42,7 +54,7 @@ class CrossFoldValidation(object):
 
             train_dataset = SubsetDataset(dataset=self.dataset,
                                           indices=train_indices)
-            valid_dataset = SubsetDataset(dataset=self.dataset,
+            valid_dataset = SubsetDataset(dataset=self.valid_dataset,
                                           indices=valid_indices)
 
             yield train_dataset, valid_dataset
