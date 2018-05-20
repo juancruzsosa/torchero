52e0ed90ea41f74b9c0e0c128e1e671a46809e98
Juan Cruz Sosa <nirvguy@gmail.com>
2018-05-20 01:43:59 -0300

    Add UnsupervisedDataset adapter

--- torchtrainer/utils/data/datasets.py
+++ torchtrainer/utils/data/datasets.py
@@ -20,3 +20,28 @@ class SubsetDataset(Dataset):
 
     def __getitem__(self, idx):
         return self._dataset[self._indices[idx]]
+
+
+class UnsuperviseDataset(Dataset):
+    """ Supervised to Unsupervised Dataset adapter
+    """
+
+    def __init__(self, dataset, input_indices=[0]):
+        """ Constructor
+
+        Arguments:
+            dataset: Original dataset
+        """
+        self._dataset = dataset
+        self._indices = input_indices
+
+    def __getitem__(self, idx):
+        element = self._dataset[idx]
+
+        if len(self._indices) == 1:
+            return element[self._indices[0]]
+        else:
+            return tuple(element[i] for i in self._indices)
+
+    def __len__(self):
+        return len(self._dataset)
