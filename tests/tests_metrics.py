Fix bad averaging Average aggregator

tests/tests_metrics.py
@@ -224,13 +224,13 @@ class MSETests(BaseMetricsTests):
         self.assertMeasureEqual(meter, [(torch.ones(1,1), torch.ones(1,1))], 0)
         self.assertMeasureEqual(meter, [(torch.ones(1,1), torch.zeros(1,1))], 1)
         self.assertMeasureEqual(meter, [(torch.zeros(1,1), torch.ones(1,1))], 1)
-        self.assertMeasureEqual(meter, [(-torch.ones(1,2), torch.zeros(1,2))], 2*1)
-        self.assertMeasureEqual(meter, [(torch.zeros(1,2), -torch.ones(1,2))], 2*1)
-        self.assertMeasureEqual(meter, [(2*torch.ones(1,2), torch.zeros(1,2))], 4*2)
-        self.assertMeasureEqual(meter, [(torch.zeros(1,2), 2*torch.ones(1,2))], 4*2)
-        self.assertMeasureEqual(meter, [(-2*torch.ones(1,2), torch.zeros(1,2))], 4*2)
-        self.assertMeasureEqual(meter, [(torch.zeros(1,2), -2*torch.ones(1,2))], 4*2)
-        self.assertMeasureAlmostEqual(sqrt_meter, [(torch.zeros(1,2), -2*torch.ones(1,2))], math.sqrt(4*2))
+        self.assertMeasureEqual(meter, [(-torch.ones(1,2), torch.zeros(1,2))], 2/2)
+        self.assertMeasureEqual(meter, [(torch.zeros(1,2), -torch.ones(1,2))], 2/2)
+        self.assertMeasureEqual(meter, [(2*torch.ones(1,2), torch.zeros(1,2))], 2**2*2/2)
+        self.assertMeasureEqual(meter, [(torch.zeros(1,2), 2*torch.ones(1,2))], 2**2*2/2)
+        self.assertMeasureEqual(meter, [(-2*torch.ones(1,2), torch.zeros(1,2))], 2**2*2/2)
+        self.assertMeasureEqual(meter, [(torch.zeros(1,2), -2*torch.ones(1,2))], 2**2*2/2)
+        self.assertMeasureAlmostEqual(sqrt_meter, [(torch.zeros(1,2), -2*torch.ones(1,2))], math.sqrt(2**2*2/2))
 
     def test_cannot_measure_with_1d_tensors(self):
         a = torch.Tensor([0.2])
@@ -262,18 +262,18 @@ class MSETests(BaseMetricsTests):
         self.assertMeasureEqual(meter, [(2*torch.ones(2,1), torch.zeros(2,1))], 4)
         self.assertMeasureEqual(meter, [(torch.zeros(2,1), 2*torch.ones(2,1))], 4)
         self.assertMeasureEqual(meter, [(torch.zeros(2,1), 2*torch.ones(2,1))], 4)
-        self.assertMeasureAlmostEqual(meter, [(torch.arange(0, 3).view(3, 1), torch.arange(3, 6).view(3, 1))], 3**2)
-        self.assertMeasureAlmostEqual(sqrt_meter, [(torch.arange(0, 3).view(3, 1), torch.arange(3, 6).view(3, 1))], 3)
+        self.assertMeasureAlmostEqual(meter, [(torch.arange(0, 3).float().view(3, 1), torch.arange(3, 6).float().view(3, 1))], 3**2)
+        self.assertMeasureAlmostEqual(sqrt_meter, [(torch.arange(0, 3).float().view(3, 1), torch.arange(3, 6).float().view(3, 1))], 3)
 
     def test_meter_value_average_over_sum_of_measured_batch_dimentions(self):
         meter = meters.MSE()
         sqrt_meter = meters.RMSE()
         self.assertMeasureAlmostEqual(meter, [(torch.ones(2,1), torch.zeros(2,1)),
-                                              (2*torch.ones(2,1), torch.zeros(2,1)),
-                                              (torch.arange(0, 3).view(3, 1), torch.arange(3, 6).view(3, 1))], (2*1**2 + 2*2**2 + 3*3**2)/7)
+                                              (2*torch.ones(2,1), torch.zeros(2,1))],
+                                      (2*1**2 + 2*2**2)/4)
         self.assertMeasureAlmostEqual(sqrt_meter, [(torch.ones(2,1), torch.zeros(2,1)),
-                                              (2*torch.ones(2,1), torch.zeros(2,1)),
-                                              (torch.arange(0, 3).view(3, 1), torch.arange(3, 6).view(3, 1))], math.sqrt((2*1**2 + 2*2**2 + 3*3**2)/7))
+                                                   (2*torch.ones(2,1), torch.zeros(2,1))],
+                                      math.sqrt((2*1**2 + 2*2**2)/4))
 
     def test_cannot_measure_with_different_type_of_tensors(self):
         import numpy as np

torchtrainer/meters/aggregators/batch.py
@@ -15,11 +15,20 @@ class Sum(BatchAggregator):
     def combine_batch(self, old_result, value):
         return old_result + torch.sum(value)
 
-class Average(Sum):
+class Average(BatchAggregator):
+    """ Aggregator that return the sums of all given batchs
+    """
+    def initial_value(self):
+        return 0
+
+    def combine_batch(self, old_result, value):
+        return old_result + torch.sum(value, dim=0)
+
     """ Aggregator that returns the average of all given batchs
     """
     def final_value(self, result):
-        return super(Average, self).final_value(result) / self.num_samples
+        result = super(Average, self).final_value(result)
+        return torch.sum(result) / (self._num_samples * result.nelement())
 
 class Maximum(BatchAggregator):
     """ Aggregator that returns the maximum of all given batchs

torchtrainer/meters/categorical_accuracy.py
@@ -26,7 +26,7 @@ class CategoricalAccuracy(_CategoricalAccuracy):
         self.k = k
 
     def _get_result(self, a, b):
-        return (a.topk(k=self.k, dim=1)[1] == b.unsqueeze(-1))
+        return torch.sum((a.topk(k=self.k, dim=1)[1] == b.unsqueeze(-1)).float(), dim=1)
 
 class BinaryAccuracy(_CategoricalAccuracy):
     """ Meter for accuracy on binary targets (assuming normalized inputs)