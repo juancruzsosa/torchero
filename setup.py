1146600ebfdfb62e7d5fd07c75018a66dd060f43
Juan Cruz Sosa <nirvguy@gmail.com>
2020-07-06 01:52:15 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='torchero',
-      version='0.1',
+      version='0.0.1',
       description='A pluggable & extensible trainer for pytorch',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/juancruzsosa/torchero',
       author='Juan Cruz Sosa',
       author_email='juancruzsosa.92@gmail.com',
@@ -13,5 +18,10 @@ setup(name='torchero',
           'pyyaml',
           'tqdm',
           'requests'
+      ],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
       ],
       zip_safe=False)
