67e8c50e60a1cb8cca242500544b4a34412a6934
Juan Cruz Sosa <nirvguy@gmail.com>
2020-07-06 01:34:41 -0300

    Set tqdm and requests as mandatory

--- setup.py
+++ setup.py
@@ -3,17 +3,15 @@ from setuptools import setup, find_packages
 setup(name='torchero',
       version='0.1',
       description='A pluggable & extensible trainer for pytorch',
-      url='http://github.com/nirvguy/torchero',
+      url='http://github.com/juancruzsosa/torchero',
       author='Juan Cruz Sosa',
       author_email='nirvguy@gmail.com',
       license='MIT',
       packages=find_packages(),
       install_requires=[
           'torch',
-          'pyyaml'
+          'pyyaml',
+          'tqdm',
+          'requests'
       ],
-      extra_requires={
-        'monitor training/validation with progress bar support' : ['tqdm'],
-        'remote monitor callback': ['requests']
-      },
       zip_safe=False)
