b01b400145982b59a99323550b396e55e121edef
Juan Cruz Sosa <juancruzsosa.92@gmail.com>
2020-07-07 14:09:06 -0300

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/bash
-python -m unittest tests.tests_base_trainer
-python -m unittest tests.tests_data
-python -m unittest tests.tests_metrics
-python -m unittest tests.tests_trainers
-python -m unittest tests.tests_callbacks
+python -m unittest discover
