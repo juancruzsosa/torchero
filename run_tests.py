1f15b0d1d496b77af42816d30b67bd0e86b9019c
Juan Cruz Sosa <juancruzsosa.92@gmail.com>
2020-07-07 17:49:59 -0300

    Fix xml report

--- run_tests.py
+++ run_tests.py
@@ -8,12 +8,11 @@ from tests import *
 xml_file = os.environ.get('XML_RESULT', 'test_report.xml')
 
 def main():
-    with open(xml_file, 'wb+') as f:
-        unittest.main(
-            testRunner=xmlrunner.XMLTestRunner(output=f),
-            failfast=False,
-            buffer=False,
-            catchbreak=False)
+    unittest.main(
+        testRunner=xmlrunner.XMLTestRunner(output=open(xml_file, 'w+'), outsuffix = ''),
+        failfast=False,
+        buffer=False,
+        catchbreak=False)
 
 if __name__ == '__main__':
     main()
