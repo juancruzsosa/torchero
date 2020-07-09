05c9c96b0e3a75e506f1aad55106d6db5f9c7a71
Juan Cruz Sosa <juancruzsosa.92@gmail.com>
2020-07-09 20:49:09 -0300

    Move binary accuracy metrics to binary_scores

--- torchero/meters/__init__.py
+++ torchero/meters/__init__.py
@@ -2,9 +2,10 @@ from .base import BaseMeter
 from .exceptions import ZeroMeasurementsError
 from .batch import BatchMeter
 from .nullmeter import NullMeter
-from .categorical_accuracy import CategoricalAccuracy, \
-                                  BinaryAccuracy, \
-                                  BinaryWithLogitsAccuracy
+from .categorical_accuracy import CategoricalAccuracy
+from .binary_scores import BinaryAccuracy, \
+                           BinaryWithLogitsAccuracy
+
 from .mse import MSE, RMSE, MSLE, RMSLE
 from .averager import Averager
 from .loss import LossMeter
