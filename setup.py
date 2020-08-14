b59ad0ac85954d287512635eb13d86f7c3a2fc99
Juan Cruz Sosa <juancruzsosa.92@gmail.com>
2020-08-14 00:38:39 -0300

    Version 0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='torchero',
-      version='0.0.4',
+      version='0.0.5',
       description='A pluggable & extensible trainer for pytorch',
       long_description=long_description,
       long_description_content_type="text/markdown",
