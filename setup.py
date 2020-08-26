6dd9c1f5d31cc2cc04e2a19e965d335c64521a65
Juan Cruz Sosa <juancruzsosa.92@gmail.com>
2020-08-25 23:02:06 -0300

    Version 0.0.6

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='torchero',
-      version='0.0.5',
+      version='0.0.6',
       description='A pluggable & extensible trainer for pytorch',
       long_description=long_description,
       long_description_content_type="text/markdown",
       url='http://github.com/juancruzsosa/torchero',
+      project_urls={
+          'Documentation': 'https://torchero.readthedocs.io'
+      },
       author='Juan Cruz Sosa',
       author_email='juancruzsosa.92@gmail.com',
       license='MIT',
@@ -26,4 +29,7 @@ setup(name='torchero',
           "License :: OSI Approved :: MIT License",
           "Operating System :: OS Independent",
       ],
+      extras_require = {
+          'export to dataframe':  ["pandas"],
+      }
       zip_safe=False)
