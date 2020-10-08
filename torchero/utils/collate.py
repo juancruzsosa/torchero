8ffe4338572e34f8aa951d77b7055741a5f8e79d
Juan Cruz Sosa <juancruzsosa.92@gmail.com>
2020-10-08 19:39:20 -0300

    Add bag of words collator class

--- torchero/utils/collate.py
+++ torchero/utils/collate.py
@@ -35,3 +35,28 @@ class PadSequenceCollate(object):
         else:
             raise RuntimeError("Labels type not supported")
         return (sequences, lengths), labels
+
+class BoWCollate(object):
+    """ Batch Collator intended to be used with EmbeddingBag
+
+    Example:
+        >> collate = BoWCollate()
+        >> collate(torch.tensor([([1, 2], torch.tensor(1.)),
+                                 ([3], torch.tensor(0))])
+        (torch.tensor([1, 2, 3]), torch.LongTensor([0, 2])), torch.tensor([1, 0])
+    """
+    def __call__(self, batch):
+        target_elem = batch[0][1]
+        if torch.is_tensor(target_elem):
+            labels = torch.stack([x[1] for x in batch])
+        elif isinstance(target_elem, np.ndarray):
+            labels = torch.from_numpy(np.stack([x[1] for x in batch]))
+        elif isinstance(target_elem, (float, int)):
+            labels = torch.tensor([x[1] for x in batch])
+        else:
+            raise RuntimeError("Labels type not supported")
+        texts = [entry[0] for entry in batch]
+        offsets = [0] + [len(x) for x in texts]
+        offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
+        texts = torch.cat(texts)
+        return (texts, offsets), labels
