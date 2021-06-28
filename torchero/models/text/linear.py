f64dca856a75e2961859c9cd3752875c658f85ad
Juan Cruz Sosa <juancruzsosa.92@gmail.com>
2021-06-28 01:59:42 -0300

    Add number of outputs to LinearModel

--- torchero/models/text/linear.py
+++ torchero/models/text/linear.py
@@ -1,14 +1,13 @@
 from torch import nn
 
 class LinearModel(nn.Module):
-    def __init__(self, vocab_size, embedding_dim):
+    def __init__(self, vocab_size, embedding_dim, output_size):
         super(LinearModel, self).__init__()
         self.embeddings = nn.EmbeddingBag(vocab_size,
                                           embedding_dim=embedding_dim)
-        self.linear = nn.Linear(embedding_dim, 1)
+        self.linear = nn.Linear(embedding_dim, output_size)
         
     def forward(self, x, offsets):
         x = self.embeddings(x, offsets)
         x = self.linear(x)
-        x = x.flatten()
         return x
