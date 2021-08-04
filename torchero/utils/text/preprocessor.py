514a5b9baa9fcbcedef2ea5b9536739266e1b32f
Juan Cruz Sosa <juancruzsosa.92@gmail.com>
2021-08-04 04:01:02 -0300

    Add support for truncation operator to TexTransform

--- torchero/utils/text/preprocessor.py
+++ torchero/utils/text/preprocessor.py
@@ -2,6 +2,7 @@ import torch
 
 from torchero.utils.text.tokenizers import tokenizers
 from torchero.utils.text.vocab import Vocab
+from torchero.utils.text.truncate import LeftTruncator, RightTruncator, CenterTruncator
 
 class TextTransform(object):
     def __init__(self,
@@ -10,6 +11,8 @@ class TextTransform(object):
                  vocab=None,
                  vocab_max_size=None,
                  vocab_min_count=1,
+                 max_len=None,
+                 truncate_mode='left',
                  bos=None,
                  eos=None,
                  pad="<pad>",
@@ -19,6 +22,17 @@ class TextTransform(object):
         if isinstance(tokenizer, str):
             tokenizer = tokenizers[tokenizer]()
         self.tokenizer = tokenizer
+        if max_len is not None:
+            if truncate_mode == 'left':
+                self.truncator = LeftTruncator(max_len)
+            elif truncate_mode == 'right':
+                self.truncator = RightTruncator(max_len)
+            elif truncate_mode == 'center':
+                self.truncator = CenterTruncator(max_len)
+            else:
+                raise ValueError("Invalid truncate_mode. Choose from left, right, center")
+        else:
+            self.truncator = None
         if vocab is None:
             vocab = Vocab(bos=bos,
                   eos=eos,
@@ -33,6 +47,8 @@ class TextTransform(object):
     def __call__(self, text):
         text = self.pre_process(text)
         tokens = self.tokenizer(text)
+        if self.truncator is not None:
+            tokens = self.truncator(tokens)
         ids = self.vocab(tokens)
         ids = self.post_process(ids)
         return ids
---
+++ torchero/utils/text/truncate.py
@@ -0,0 +1,39 @@
+import math
+from abc import abstractmethod, ABCMeta
+
+class Truncator(object, metaclass=ABCMeta):
+    """ Truncates the input token sequence
+    """
+    def __init__(self, max_len):
+        if max_len < 0:
+            raise ValueError("max_len should be positive")
+        self.max_len = max_len
+
+    @abstractmethod
+    def truncate(self, tokens):
+        pass
+
+    def __call__(self, tokens):
+        if len(tokens) <= self.max_len:
+            return tokens
+        else:
+            return self.truncate(tokens)
+
+class LeftTruncator(Truncator):
+    """ Truncates right part of the input token sequence
+    """
+    def truncate(self, tokens):
+        return tokens[:self.max_len]
+
+class RightTruncator(Truncator):
+    """ Truncates left part of the input token sequence
+    """
+    def truncate(self, tokens):
+        return tokens[-self.max_len:]
+
+class CenterTruncator(Truncator):
+    """ Truncates center part of the input token sequence
+    """
+    def truncate(self, tokens):
+        mid = len(tokens)//2
+        return tokens[mid-math.floor(self.max_len/2):mid+math.ceil(self.max_len/2)]
