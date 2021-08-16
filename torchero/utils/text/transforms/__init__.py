a5f38ea63fd71e03849858007ce2f7c21a73fdf4
Juan Cruz Sosa <juancruzsosa.92@gmail.com>
2021-08-16 14:06:58 -0300

    Add Vocab and OutOfVocabularyError to transforms.__init__

--- torchero/utils/text/transforms/__init__.py
+++ torchero/utils/text/transforms/__init__.py
@@ -1,5 +1,6 @@
 from torch import LongTensor as ToTensor
 
+from torchero.utils.text.transforms.vocab import Vocab, OutOfVocabularyError
 from torchero.utils.text.transforms.compose import Compose
 from torchero.utils.text.transforms.tokenizers import *
 from torchero.utils.text.transforms.truncate import *
